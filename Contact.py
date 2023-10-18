78393e69bfaf283f633a93efda5d7fbfce70f00e
iamLogesh <clogeshwaran@yahoo.com>
2023-10-18 19:23:22 +0530

    removed streamlit

--- Contact.py
+++ Contact.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import streamlit as st
 
 # Function to create a table for storing contacts
 def create_contact_table(cursor):
@@ -12,54 +11,51 @@ def create_contact_table(cursor):
     """
     cursor.execute(create_table_query)
     mydb.commit()
-    st.write("Contact table created successfully!")
+    print("Contact table created successfully!")
 
 # Function to create a new contact
 def create_contact(cursor):
-    name = st.text_input("Enter the contact's name:")
-    phone_number = st.text_input("Enter the contact's phone number:")
-    if st.button("Add Contact"):
-        insert_query = "INSERT INTO contacts (name, phone_number) VALUES (%s, %s)"
-        cursor.execute(insert_query, (name, phone_number))
-        mydb.commit()
-        st.write("Contact added successfully!")
+    name = input("Enter the contact's name: ")
+    phone_number = input("Enter the contact's phone number: ")
+    insert_query = "INSERT INTO contacts (name, phone_number) VALUES (%s, %s)"
+    cursor.execute(insert_query, (name, phone_number))
+    mydb.commit()
+    print("Contact added successfully!")
 
 # Function to view all contacts
 def view_contacts(cursor):
     cursor.execute("SELECT * FROM contacts")
     contacts = cursor.fetchall()
     if not contacts:
-        st.write("No contacts found.")
+        print("No contacts found.")
     else:
-        st.write("Contacts:")
+        print("Contacts:")
         for contact in contacts:
-            st.write(f"Name: {contact[1]}, Phone Number: {contact[2]}")
+            print(f"Name: {contact[1]}, Phone Number: {contact[2]}")
 
 # Function to delete a contact
 def delete_contact(cursor):
-    phone_number = st.text_input("Enter the phone number of the contact to delete:")
-    if st.button("Delete Contact"):
-        delete_query = "DELETE FROM contacts WHERE phone_number = %s"
-        cursor.execute(delete_query, (phone_number,))
-        mydb.commit()
-        if cursor.rowcount > 0:
-            st.write("Contact deleted successfully!")
-        else:
-            st.write("Contact not found.")
+    phone_number = input("Enter the phone number of the contact to delete: ")
+    delete_query = "DELETE FROM contacts WHERE phone_number = %s"
+    cursor.execute(delete_query, (phone_number,))
+    mydb.commit()
+    if cursor.rowcount > 0:
+        print("Contact deleted successfully!")
+    else:
+        print("Contact not found.")
 
 # Function to search for a contact by name or phone number
 def search_contact(cursor):
-    keyword = st.text_input("Enter the name or phone number to search for:")
-    if st.button("Search"):
-        search_query = "SELECT * FROM contacts WHERE name LIKE %s OR phone_number LIKE %s"
-        cursor.execute(search_query, (f'%{keyword}%', f'%{keyword}%'))
-        contacts = cursor.fetchall()
-        if not contacts:
-            st.write("No matching contacts found.")
-        else:
-            st.write("Matching Contacts:")
-            for contact in contacts:
-                st.write(f"Name: {contact[1]}, Phone Number: {contact[2]}")
+    keyword = input("Enter the name or phone number to search for: ")
+    search_query = "SELECT * FROM contacts WHERE name LIKE %s OR phone_number LIKE %s"
+    cursor.execute(search_query, (f'%{keyword}%', f'%{keyword}%'))
+    contacts = cursor.fetchall()
+    if not contacts:
+        print("No matching contacts found.")
+    else:
+        print("Matching Contacts:")
+        for contact in contacts:
+            print(f"Name: {contact[1]}, Phone Number: {contact[2]}")
 
 # Establish a connection to the MySQL database
 mydb = mysql.connector.connect(
@@ -69,24 +65,37 @@ mydb = mysql.connector.connect(
     database="pybase"
 )
 
+if mydb.is_connected():
+    print("Connected to the MySQL database.")
+
 cursor = mydb.cursor()
 
 # Call the function to create the contact table
 create_contact_table(cursor)
 
-# Streamlit app
-st.title("Telephone Directory")
+# Text-based menu
+while True:
+    print("\nOptions:")
+    print("1. Create Contact")
+    print("2. View Contacts")
+    print("3. Delete Contact")
+    print("4. Search Contact")
+    print("5. Exit")
 
-option = st.sidebar.selectbox("Select an option:", ["Create New Contact", "View Contacts", "Delete Contact", "Search Contact"])
+    choice = input("Enter your choice: ")
 
-if option == "Create New Contact":
-    create_contact(cursor)
-elif option == "View Contacts":
-    view_contacts(cursor)
-elif option == "Delete Contact":
-    delete_contact(cursor)
-elif option == "Search Contact":
-    search_contact(cursor)
+    if choice == "1":
+        create_contact(cursor)
+    elif choice == "2":
+        view_contacts(cursor)
+    elif choice == "3":
+        delete_contact(cursor)
+    elif choice == "4":
+        search_contact(cursor)
+    elif choice == "5":
+        break
+    else:
+        print("Invalid choice. Please select a valid option.")
 
 # Close the cursor and the database connection
 cursor.close()
